302bf2713dd57202297b1f27ad4ac7b1f13730c8
Achal Ahir <achala.se25@sbjit.edu.in>
2026-05-04 22:07:19 +0530

    Part1

--- 3rd part.js
+++ 3rd part.js
@@ -1,11 +1,16 @@
+
 window.onload = function() {
 
+ 
   var form = document.getElementById("contactForm");
 
+ 
   form.onsubmit = function(event) {
 
+   
     event.preventDefault();
 
+    
     var name    = document.getElementById("fullname").value;
     var email   = document.getElementById("email").value;
     var phone   = document.getElementById("phone").value;
@@ -18,8 +23,10 @@ window.onload = function() {
     localStorage.setItem("userService", service);
     localStorage.setItem("userMessage", message);
 
+    
     document.getElementById("successMsg").style.display = "block";
 
+   
     document.getElementById("showName").innerHTML    = localStorage.getItem("userName");
     document.getElementById("showEmail").innerHTML   = localStorage.getItem("userEmail");
     document.getElementById("showPhone").innerHTML   = localStorage.getItem("userPhone") || "Not provided";
